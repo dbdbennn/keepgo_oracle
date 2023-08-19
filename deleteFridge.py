d07493004047fe5eadbf869da841377ed6122077
Yu Jung Eun <0hh.0553@gmail.com>
2023-08-19 09:36:01 +0900

    기한별로 갯수보기(view, subquery)

--- deleteFridge.py
+++ deleteFridge.py
@@ -5,35 +5,39 @@ def deleteFridge(cursor):
     print()
     print(sc.str_Cyan("냉장고에서 음식 꺼내기 - 🍅 - 🥕 - 🥬 - 🥩 - 🥚 - 🍇 - 🥔 - 🍠"))
 
-    cursor.execute("SELECT food_name FROM Fridge")
-    fridge = [row[0] for row in cursor.fetchall()]
+    cursor.execute("SELECT food_name, food_pieces FROM Fridge")
+    fridge_data = {row[0]: row[1] for row in cursor.fetchall()}
 
-    if len(fridge) == 0:
+    if not fridge_data:
         print("\n\t  \033[31m❗ 음식이 없어 꺼낼 수 없습니다.\033[0m")
+        return
+
     name = input("\n\t\t꺼낼 음식은? > ")
-    while name not in fridge:
+    while name not in fridge_data:
         print(sc.str_Red("\n\t\t\033[31m❗ 입력한 음식이 없습니다.\033[0m"))
         name = input("\n\t\t꺼낼 음식은? > ")
 
+    available_pieces = fridge_data[name]
+
     amount = int(input("\n\t\t꺼낼 음식의 갯수는? > "))
-    while amount <= 0:
-        print(sc.str_Red("\n\t\t\033[31m❗ 입력한 음식의 갯수가 올바르지 않습니다.\033[0m"))
+    while amount <= 0 or amount > available_pieces:
+        if amount <= 0:
+            print(sc.str_Red("\n\t\t\033[31m❗ 입력한 음식의 갯수가 올바르지 않습니다.\033[0m"))
+        else:
+            print(sc.str_Red("\n\t\t\033[31m❗ 초과하여 꺼낼 수 없습니다.\033[0m"))
         amount = int(input("\n\t\t꺼낼 음식의 갯수는? > "))
 
     cursor.execute(
-        "SELECT food_pieces FROM Fridge WHERE food_name = :name", {"name": name}
+        "UPDATE Fridge SET food_pieces = food_pieces - :amount WHERE food_name = :name",
+        {"amount": amount, "name": name},
     )
-    current_pieces = cursor.fetchone()[0]
-    if amount >= current_pieces:
+
+    if available_pieces == amount:
         cursor.execute("DELETE FROM Fridge WHERE food_name = :name", {"name": name})
-    else:
-        cursor.execute(
-            "UPDATE Fridge SET food_pieces = food_pieces - :amount WHERE food_name = :name",
-            {"amount": amount, "name": name},
-        )
+
     cursor.connection.commit()
 
-    if amount >= current_pieces:
+    if available_pieces == amount:
         print("\n\t\t" + sc.str_Blue(name) + "을(를) 모두 꺼냈습니다!")
     else:
         print("\n\t\t" + sc.str_Blue(name) + "을(를) " + str(amount) + "개 꺼냈습니다!")
--- exdateFridge.py
+++ exdateFridge.py
@@ -2,23 +2,20 @@ from tabulate import tabulate
 import strChanger as sc
 
 
-def create_expiration_view(new_cursor):
-    create_view_query = """
-        CREATE OR REPLACE VIEW ExpirationCountsView AS
-        SELECT
-            SUM(CASE WHEN expiration_date - SYSDATE <= 7 THEN 1 ELSE 0 END) AS within_7_days,
-            SUM(CASE WHEN expiration_date - SYSDATE <= 30 THEN 1 ELSE 0 END) AS within_30_days,
-            SUM(CASE WHEN expiration_date < SYSDATE THEN 1 ELSE 0 END) AS expired
-        FROM Fridge
-    """
-    new_cursor.execute(create_view_query)
-
-
 def exdateFridge(new_cursor):
     print()
     print(sc.str_Yellow("기한별로 갯수보기 * 🍅 * 🥕 * 🥬 * 🥩 * 🥚 * 🍇 * 🥔 * 🧀"))
 
-    create_expiration_view(new_cursor)
+    create_view_query = """
+        CREATE OR REPLACE VIEW ExpirationCountsView AS
+            SELECT
+                SUM(CASE WHEN expiration_date < SYSDATE THEN food_pieces ELSE 0 END) AS expired,
+                SUM(CASE WHEN expiration_date - SYSDATE <= 7 AND expiration_date - SYSDATE >= 1 THEN food_pieces ELSE 0 END) AS within_7_days,
+                SUM(CASE WHEN expiration_date - SYSDATE <= 30 AND expiration_date - SYSDATE >= 8 THEN food_pieces ELSE 0 END) AS within_30_days,
+                SUM(CASE WHEN expiration_date > SYSDATE + 30 THEN food_pieces ELSE 0 END) AS more_than_30_days
+            FROM Fridge
+        """
+    new_cursor.execute(create_view_query)
 
     select_view_query = "SELECT * FROM ExpirationCountsView"
     new_cursor.execute(select_view_query)
@@ -26,12 +23,13 @@ def exdateFridge(new_cursor):
 
     print()
     table_data = [
-        ["유통기한 지남", expiration_counts[2]],
-        ["7일 이하", expiration_counts[0]],
-        ["한달 이하", expiration_counts[1]],
+        ["유통기한 지남", expiration_counts[0]],
+        ["7일 이하", expiration_counts[1]],
+        ["한달 이하", expiration_counts[2]],
+        ["한달 이상", expiration_counts[3]],
     ]
 
-    table_headers = ["남은기간", "음식 갯수"]
+    table_headers = ["남은기한", "음식 갯수"]
     table = tabulate(table_data, headers=table_headers, tablefmt="rounded_grid")
 
     print(table)
